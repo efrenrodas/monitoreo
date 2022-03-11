65ed43cddcb626d0d396d06563b1f4edc00700bd
desarrollo SI <desarrollo@sanisidro.edu.ec>
2022-03-11 15:56:15 -0500

    guardar registros

---
+++ app/Http/Controllers/ApiRegistroController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Registro;
+use Illuminate\Http\Request;
+
+class ApiRegistroController extends Controller
+{
+    //
+    public function guarda(Request $request){
+        
+        request()->validate(Registro::$rules);
+
+        $registro = Registro::create($request->all());
+
+        return response()->json($registro, 200);
+    }
+}
